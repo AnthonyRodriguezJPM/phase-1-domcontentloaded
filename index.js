d07250d31e44d712e67b280b4dde5f715c159f24
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-29 01:28:54 -0500

    commit tree c7bde9e1e27be22462f7ec4b7e55437c9b04d26c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('The DOM has loaded');
+
+    let change = document.querySelector('p')
+
+    change.textContent='This is really cool!';
+
+    console.log(change);
+    
+
+    
+});
+
+
+
